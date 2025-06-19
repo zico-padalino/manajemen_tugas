214061784ab34da8524dbcae84e65368b23e0df9
zico-padalino <zico.padalino@untirta.ac.id>
2025-06-19 16:29:16 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+route::get ('dashboard',[DashboardController::class,'index']);
+    
